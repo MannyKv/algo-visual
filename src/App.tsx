7862e166699922968343132ae6d882af55eecd27
Manav Lal <manavlal5@gmail.com>
2023-10-03 02:27:48 +1300

    Created the base functionlity for sorting. This must be improved on and moved to its own file and also animated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Box, Heading, Flex, Button, Link, Grid, Text, GridItem} from "@chakra-ui/react";
 import Visualiser from "./components/Visualiser";
-const data = [10,20,30,50,40];
+import BubbleSort from "./algorithms/BubbleSort";
 function App() {
+    const [data, setData] = useState([100,200,300,500,400,50,20,500,550,600]);
+
+    const [sorting, setSorting] = useState(false);
+
+    const BubbleSort = ()=>{
+        setTimeout(() => {
+             let newArr = [...data];
+            for (let i = 0; i < data.length - 1; i++) {
+                setTimeout(() => {
+                    for (let j = i + 1; j < data.length; j++) {
+                        if (newArr[i] > newArr[j]) {
+                            let temp = newArr[i];
+                            newArr[i] = newArr[j];
+                            newArr[j] = temp;
+                            let newStep = [...newArr];
+                            setTimeout(() => {
+                                setData([...newStep]);
+                            }, j * 100);
+                        }
+
+                    }
+                }, i * 1000);
+            }
+        }, 500);
+
+    };
     return (
         <Grid
             h={"100vh"}
@@ -23,7 +49,7 @@ function App() {
                     <Text fontSize={'2xl'}
                           color={"#F2E9E4"}
                           fontFamily={"Montserrat"}>Choose The Sort Algorithm</Text>
-
+                    <Button onClick={BubbleSort}></Button>
 
                 </Flex>
             </GridItem>
